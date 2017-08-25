6eed89b1e729bcd8f4a466bfb587693502b986b2
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-08-25 09:31:54 -0300

    release 1

--- database/seeds/PacientesSeed.php
+++ database/seeds/PacientesSeed.php
@@ -12,8 +12,8 @@ class PacientesSeed extends Seeder
      */
     public function run()
     {
-        Pacientes::insert(['nome_paciente' => 'test_1', 'data_nascimento' => '1988-07-08', 'cod_paciente'=> '10', 'sexo'=>'1', 'estado_civil' => 'Solteiro', 'cpf'=> '126.135.168-83']);
-        Pacientes::insert(['nome_paciente' => 'test_2', 'data_nascimento' => '1978-07-08', 'cod_paciente'=> '11', 'sexo'=>'2', 'estado_civil' => 'Casado', 'cpf'=> '126.135.118-82']);
-        Pacientes::insert(['nome_paciente' => 'test_3', 'data_nascimento' => '1968-07-08', 'cod_paciente'=> '12', 'sexo'=>'1', 'estado_civil' => 'hue', 'cpf'=> '121.125.368-83']);
+        Pacientes::insert(['nome_paciente' => 'test_1', 'data_nascimento' => '1999-07-08', 'cod_paciente'=> '10', 'sexo'=>'1', 'estado_civil' => 'Solteiro', 'cpf'=> '116.135.168-81']);
+        Pacientes::insert(['nome_paciente' => 'test_2', 'data_nascimento' => '1978-07-08', 'cod_paciente'=> '11', 'sexo'=>'2', 'estado_civil' => 'Casado', 'cpf'=> '136.135.118-89']);
+        Pacientes::insert(['nome_paciente' => 'test_3', 'data_nascimento' => '1968-07-08', 'cod_paciente'=> '12', 'sexo'=>'1', 'estado_civil' => 'hue', 'cpf'=> '121.125.368-80']);
     }
 }
